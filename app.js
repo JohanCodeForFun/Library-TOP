a1109550b1e2d98167602d08f420f7862839ad3b
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-12-17 05:01:27 +0100

    added input forms, queryselector and listening for input

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-console.log('typescript transpiled');
 var myLibrary = [];
-function Book() {
-    // the constructor...
-}
-function addBookToLibrary() {
-    // do stuff here
+var inputTitle = document.querySelector('#inputTitle');
+var inputAuthor = document.querySelector('#inputAuthor');
+var bookParagraph = document.querySelector('#bookParagraph');
+var submitButton = document.querySelector('#submitBook');
+// function to listen for input and update live in console.
+// inputTitle?.addEventListener('input', () => console.log(`${inputTitle.value}`))
+var addBookToLibrary = function () {
+    bookParagraph === null || bookParagraph === void 0 ? void 0 : bookParagraph.textContent = "Added book, ".concat(inputTitle === null || inputTitle === void 0 ? void 0 : inputTitle.value, " ").concat(inputAuthor === null || inputAuthor === void 0 ? void 0 : inputAuthor.value);
+    // let count = 0;
+    // const book1 = new (Book as any)('Amazing Title', 'John Doe')
+    // // do stuff here
+    // console.log(book1)
+};
+submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener('click', addBookToLibrary);
+function Book(title, author) {
+    this.title,
+        this.author,
+        this.bookInfo = function () {
+            return; // ... 
+        };
 }
--- app.ts
+++ app.ts
@@ -1,11 +1,26 @@
-console.log('typescript transpiled');
-
 let myLibrary = [];
 
-function Book() {
-	// the constructor...
+const inputTitle = document.querySelector('#inputTitle');
+const inputAuthor = document.querySelector('#inputAuthor');
+const bookParagraph = document.querySelector('#bookParagraph');
+const submitButton = document.querySelector('#submitBook');
+
+// function to listen for input and update live in console.
+// inputTitle?.addEventListener('input', () => console.log(`${inputTitle.value}`))
+
+const addBookToLibrary = () => {
+	bookParagraph?.textContent = `Added book, ${inputTitle?.value} ${inputAuthor?.value}`
+	// let count = 0;
+	// const book1 = new (Book as any)('Amazing Title', 'John Doe')
+	// // do stuff here
+	// console.log(book1)
 }
 
-function addBookToLibrary() {
-	// do stuff here
+submitButton?.addEventListener('click', addBookToLibrary)
+function Book(this:any, title:string, author:string) {
+	this.title,
+	this.author,
+	this.bookInfo = function() {
+		return // ... 
+	}
 }
