4941fda425e99b370ed1200fcf4b4b1a9d4d57a0
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-12-18 19:20:32 +0100

    add book to librart works!

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ var inputPages = document.querySelector('#inputPages');
 var bookParagraph = document.querySelector('#bookParagraph');
 var submitButton = document.querySelector('#submitBook');
 var booksBody = document.querySelector('#booksBody');
-// function to listen for input and update live in console.
-// inputTitle?.addEventListener('input', () => console.log(`${inputTitle.value}`))
-var addBookToLibrary = function () {
-    bookParagraph === null || bookParagraph === void 0 ? void 0 : bookParagraph.textContent = "Added book, ".concat(inputTitle === null || inputTitle === void 0 ? void 0 : inputTitle.value, " ").concat(inputAuthor === null || inputAuthor === void 0 ? void 0 : inputAuthor.value, " ").concat(inputPages === null || inputPages === void 0 ? void 0 : inputPages.value);
-    var id = 1;
-    // const book1 = new (Book as any)('Amazing Title', 'John Doe')
-    // // do stuff here
-    // console.log(book1)
-};
-submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener('click', addBookToLibrary);
 function Book(title, author, pages, id, read) {
     this.title = title, // = inputTitle?.value,
         this.author = author, // = inputAuthor?.value,
@@ -26,19 +16,13 @@ function Book(title, author, pages, id, read) {
         return; // ... 
     };
 }
-var book1 = new Book("javascript Funktioner", "Johan Hellberg", 900, 1, true);
-console.log(book1);
-var book2 = new Book("javascript OOP", "Johan Hellberg", 300, 1, true);
-console.log(book1);
-var book3 = new Book("javascript ES6", "Johan Hellberg", 500, 1, true);
-console.log(book1);
-var bookData = [book1, book2, book3];
-// example how to display data with .map
-// https://stackoverflow.com/questions/14643617/create-table-using-javascript
-var data = [{ Name: 'Sydney', Day: 'Monday', Time: '10:00AM' }, { Name: 'New York', Day: 'Monday', Time: '11:00AM' }, { Name: 'Sydney', Day: 'Monday', Time: '10:00AM' }, { Name: 'New York', Day: 'Monday', Time: '11:00AM' }, { Name: 'Sydney', Day: 'Monday', Time: '10:00AM' }, { Name: 'New York', Day: 'Monday', Time: '11:00AM' },]; // any json data or array of objects
-var tabelData = bookData.map(function (value) {
-    return ("<tr>\n\t<td> ".concat(value.title, " </td>\n\t<td> ").concat(value.author, " </td>\n\t<td> ").concat(value.pages, " </td>\n\t<td> ").concat(value.read, " </td>\n</tr>"));
-}).join('');
-booksBody === null || booksBody === void 0 ? void 0 : booksBody.innerHTML = tabelData;
-// bookParagraph?.textContent = book1.title
-var book1content = "\n<tr>\n\t<td> ".concat(book1.title, " </td>\n\t<td> ").concat(book1.author, " </td>\n\t<td> ").concat(book1.pages, " </td>\n\t<td> ").concat(book1.read, " </td>\n</tr>");
+var addBookToLibrary = function () {
+    var id = 1;
+    myLibrary.push(new Book("".concat(inputTitle.value), "".concat(inputAuthor.value), "".concat(inputPages.value), "".concat(id), true));
+    var tabelData = myLibrary.map(function (value) {
+        return ("<tr>\n\t\t<td> ".concat(value.title, " </td>\n\t\t<td> ").concat(value.author, " </td>\n\t\t<td> ").concat(value.pages, " </td>\n\t\t<td> ").concat(id, " </td>\n\t</tr>"));
+    }).join('');
+    booksBody === null || booksBody === void 0 ? void 0 : booksBody.innerHTML = tabelData;
+};
+// function to listen for input and update live in console.
+submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener('click', addBookToLibrary);
--- app.ts
+++ app.ts
@@ -9,18 +9,6 @@ const bookParagraph = document.querySelector('#bookParagraph');
 const submitButton = document.querySelector('#submitBook');
 const booksBody = document.querySelector('#booksBody');
 
-// function to listen for input and update live in console.
-// inputTitle?.addEventListener('input', () => console.log(`${inputTitle.value}`))
-
-const addBookToLibrary = () => {
-	bookParagraph?.textContent = `Added book, ${inputTitle?.value} ${inputAuthor?.value} ${inputPages?.value}`
-	let id = 1;
-	// const book1 = new (Book as any)('Amazing Title', 'John Doe')
-	// // do stuff here
-	// console.log(book1)
-}
-
-submitButton?.addEventListener('click', addBookToLibrary)
 function Book(this:any, title:string, author:string, pages:number, id:number, read:boolean) {
 	this.title = title, // = inputTitle?.value,
 	this.author = author, // = inputAuthor?.value,
@@ -33,68 +21,27 @@ function Book(this:any, title:string, author:string, pages:number, id:number, re
 	}
 }
 
-let book1 = new Book("javascript Funktioner", "Johan Hellberg", 900, 1, true)
-console.log(book1)
-let book2 = new Book("javascript OOP", "Johan Hellberg", 300, 1, true)
-console.log(book1)
-let book3 = new Book("javascript ES6", "Johan Hellberg", 500, 1, true)
-console.log(book1)
+const addBookToLibrary = () => {
+	let id = 1;
 
-let bookData = [book1, book2, book3];
-
-// example how to display data with .map
-// https://stackoverflow.com/questions/14643617/create-table-using-javascript
-const data = [{Name:'Sydney', Day: 'Monday', Time: '10:00AM'},{Name:'New York', Day: 'Monday',Time: '11:00AM'}, {Name:'Sydney', Day: 'Monday', Time: '10:00AM'},{Name:'New York', Day: 'Monday',Time: '11:00AM'}, {Name:'Sydney', Day: 'Monday', Time: '10:00AM'},{Name:'New York', Day: 'Monday',Time: '11:00AM'},]; // any json data or array of objects
-
-const tabelData = bookData.map(value => {
-	return (
-		`<tr>
-	<td> ${value.title} </td>
-	<td> ${value.author} </td>
-	<td> ${value.pages} </td>
-	<td> ${value.read} </td>
-</tr>`
-	);
-}).join('');
-
-booksBody?.innerHTML = tabelData
+	myLibrary.push(new Book(`${inputTitle.value}`, `${inputAuthor.value}`, `${inputPages.value}`, `${id}`, true))
 
 
-// bookParagraph?.textContent = book1.title
+	const  tabelData = myLibrary.map(value => {
+		return (
+			`<tr>
+		<td> ${value.title} </td>
+		<td> ${value.author} </td>
+		<td> ${value.pages} </td>
+		<td> ${id} </td>
+	</tr>`
+		);
+	}).join('');
+	
+	booksBody?.innerHTML = tabelData
 
-let book1content = `
-<tr>
-	<td> ${book1.title} </td>
-	<td> ${book1.author} </td>
-	<td> ${book1.pages} </td>
-	<td> ${book1.read} </td>
-</tr>`
 
+}
 
-// booksBody?.innerHTML = `
-// <tr>
-// 	<td> ${book1.title} </td>
-// 	<td> ${book1.author} </td>
-// 	<td> ${book1.pages} </td>
-// 	<td> ${book1.read} </td>
-// </tr>
-// <tr>
-// 	<td> ${book1.title} </td>
-// 	<td> ${book1.author} </td>
-// 	<td> ${book1.pages} </td>
-// 	<td> ${book1.read} </td>
-// </tr>
-// <tr>
-// 	<td> ${book1.title} </td>
-// 	<td> ${book1.author} </td>
-// 	<td> ${book1.pages} </td>
-// 	<td> ${book1.read} </td>
-// </tr>
-// <tr>
-// 	<td> ${book1.title} </td>
-// 	<td> ${book1.author} </td>
-// 	<td> ${book1.pages} </td>
-// 	<td> ${book1.read} </td>
-// </tr>
-
-// `
+// function to listen for input and update live in console.
+submitButton?.addEventListener('click', addBookToLibrary)
