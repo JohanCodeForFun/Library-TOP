1955d06667780ea4677735b7563f9703f99fa0dd
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-28 20:39:29 +0200

    Add example usage

--- src/MyClassExample.ts
+++ src/MyClassExample.ts
@@ -15,3 +15,17 @@ class MyClass {
   [Symbol.iterator]() {} // method with computed name (symbol here)
   // ...
 }
+
+// Example usage
+const myBook = new Book(1, "1984", "George Orwell", 328, true);
+
+// Using getters
+console.log(myBook.title); // "1984"
+console.log(myBook.author); // "George Orwell"
+
+// Using setters
+myBook.title = "Animal Farm";
+myBook.author = "George Orwell";
+
+console.log(myBook.title); // "Animal Farm"
+console.log(myBook.author); // "George Orwell"
