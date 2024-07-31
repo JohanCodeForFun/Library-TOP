862e17eb1f2247e4d6c405a58f71232c69367891
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-07-31 23:13:23 +0200

    Update TypeScript and webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-	filename: 'bundle.js',
-	path: path.resolve(__dirname, 'dist'),
-  },
+  entry: './src/app.ts',
   module: {
-	rules: [
-	  {
-		test: /\.js$/,
-		exclude: /node_modules/,
-		use: {
-		  loader: 'ts-loader',
-		},
-	  },
-	],
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules',
+      },
+    ],
   },
-};
-
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+}
