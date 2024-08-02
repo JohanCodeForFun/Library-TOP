ba1b2499d10ae16ef1c061609da861e4d2586906
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-08-02 19:33:10 +0200

    Update webpack configuration to output html and css to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
-const path = require('path');
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/app.ts",
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: '/node_modules',
+        use: "ts-loader",
+        exclude: "/node_modules",
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
-}
+};
